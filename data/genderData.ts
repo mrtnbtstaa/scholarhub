e249536c988297c0419f791b3fee44ca78db2bf0
Vince Stephen David <stoicdavid00@gmail.com>
2026-09-01 16:46:42 +0800

    fix: gender data value capitalized for consistency

---
+++ data/genderData.ts
@@ -0,0 +1,12 @@
+import { GenericSelect } from "@/types/shared/select.types";
+
+export const GenderData: GenericSelect[] = [
+  {
+    label: "Male",
+    value: "Male",
+  },
+  {
+    label: "Female",
+    value: "Female",
+  },
+];
