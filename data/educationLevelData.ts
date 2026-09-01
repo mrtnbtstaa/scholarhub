a1e464884161d665a604405cc886a080680974c1
Vince Stephen David <stoicdavid00@gmail.com>
2026-09-01 16:46:22 +0800

    feat:  integration of education level and change variant to tertiary

---
+++ data/educationLevelData.ts
@@ -0,0 +1,26 @@
+import { GenericSelect } from "@/types/shared/select.types";
+
+export const EducationLevelData:GenericSelect[] = [
+    {
+        value: "Bachelor's Degree",
+        label: "Bachelor's Degree"
+    },
+    {
+        value: "Doctorate Degree",
+        label: "Doctorate Degree"
+    },
+    {
+        value: "Associate Degree",
+        label: "Associate Degree"
+    },
+    {
+        value: "High School",
+        label: "High School"
+    },
+    {
+        value: "Master's Degree",
+        label: "Master's Degree"
+    }
+
+
+]
--- features/student/onboarding/components/Stepper/StepTwo.tsx
+++ features/student/onboarding/components/Stepper/StepTwo.tsx
@@ -1,9 +1,13 @@
 import FormField from "@/components/ui/FormField/FormField";
-import Label from "@/components/ui/Label/Label";
 import Input from "@/components/ui/Input/Input";
 import { BiSolidInstitution } from "react-icons/bi";
+import SelectField from "@/components/shared/SelectField/SelectField";
+import { EducationLevelData } from "@/data/educationLevelData";
+import { useOnboardingStore } from "@/store/useOnboardDraftStore";
+import { Label } from "@/components/ui/label";
 
 const StepTwo = () => {
+  const {draft, updateField} = useOnboardingStore()
   return (
     <div className="w-full p-4 flex flex-col items-center justify-center">
       <div className="mt-4 text-center">
@@ -16,42 +20,50 @@ const StepTwo = () => {
       </div>
       <form className="w-full">
         <div className="mt-8 w-full">
-          <FormField className="w-full">
-            <Label htmlFor="Education Level">Education Level</Label>
-            <select className="outline-none border border-[#c5c7cf] p-3 rounded-md leading-none w-full">
-              <option defaultValue="Undergraduate">Undergraduate</option>
-            </select>
-          </FormField>
-          <FormField className="w-full">
-            <Label htmlFor="institution name">Institution Name</Label>
-            <Input
-              type="text"
-              prefixIcon={BiSolidInstitution}
-              placeholder="e.g. Stanford University"
-            />
-          </FormField>
+         <div className="flex gap-2">
+            <FormField className="w-full">
+              <Label htmlFor="Education Level">Education Level</Label>
+              <SelectField
+              variant="tertiary"
+              placeholder="Select Education Level"
+              onChange={(value) => updateField("education_level", value as string)}
+              data={EducationLevelData}
+              value={draft.education_level ?? ""}
+              className="leading-none w-full">
+              </SelectField>
+            </FormField>
+            <FormField className="w-full">
+              <Label htmlFor="institution name">Institution Name</Label>
+              <Input
+                variant="tertiary"
+                type="text"
+                prefixIcon={BiSolidInstitution}
+                placeholder="e.g. Stanford University"
+              />
+            </FormField>
+         </div>
           <div className="lg:flex grid grid-cols-1 grid-rows-1 w-full gap-2">
             <FormField className="w-full">
               <Label htmlFor="degree type">Degree Type</Label>
-              <Input type="text" placeholder="e.g. B.Sc" />
+              <Input variant="tertiary" type="text" placeholder="e.g. B.Sc" />
             </FormField>
             <FormField className="w-full">
               <Label htmlFor="Field of Study">Field of Study</Label>
-              <Input type="text" placeholder="e.g. Computer Science" />
+              <Input variant="tertiary" type="text" placeholder="e.g. Computer Science" />
             </FormField>
           </div>
           <div className="lg:flex grid grid-cols-1 grid-rows-1 w-full gap-2">
             <FormField className="w-full">
               <Label htmlFor="Current Year">Current Year</Label>
-              <Input type="text" placeholder="2nd year" />
+              <Input variant="tertiary" type="text" placeholder="2nd year" />
             </FormField>
             <FormField className="w-full">
               <Label htmlFor="Graduation">Graduation</Label>
-              <Input type="text" placeholder="2026" />
+              <Input variant="tertiary" type="text" placeholder="2026" />
             </FormField>
             <FormField className="w-full">
               <Label htmlFor="GPA">GPA</Label>
-              <Input type="text" placeholder="3.8/4.0" />
+              <Input variant="tertiary" type="text" placeholder="3.8/4.0" />
             </FormField>
           </div>
         </div>
